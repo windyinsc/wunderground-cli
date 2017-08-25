041dbf55d588c1d25b57aa3988544df3d9b7d2aa
Mirko Schubert <mirkoschubert@gmail.com>
2017-08-25 18:29:19 +0200

    Basic Structure & First API Call

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
 #!/usr/bin/env node --harmony
 'use strict'
 
-const Promise = require('promise')
-const fse = require('fs-extra')
-const chalk = require('chalk')
-const moment = require('moment')
-const app = require('commander')
+const Config = require('./lib/config');
+const Wunder = require('./lib/wunderapi');
+const app = require('commander');
+
+var cfg = new Config();
+var wunder = new Wunder(cfg.data.apikey);
 
 app
   .version('0.0.1')
-  .option('-V, --verbose', 'Switches to Verbose Mode')
+  .option('-V, --verbose', 'Switches to Verbose Mode');
+
+app
+  .command('init [key]')
+  .description('Initializes the Wunderground API')
+  .action(key => {
+
+    if (!key) {
+      console.error(new Error('You should enter a valid API key!'));
+    } else {
+      cfg.saveKey(key)
+      .then(res =>{
+        console.log('Your API key is saved successfully.');
+      })
+      .catch(err => {
+        console.error(err.message);
+      });
+    }
+  });
 
 app
   .command('search [city]')
   .alias('s')
   .description('Searches for city in wunderground')
-  .action(function (city) {
-    // search for city
-  })
+  .action((city) => {
+    
+    if (!city) {
+      console.error(new Error('You should enter a City!'));
+    } else {
+      wunder.searchCity(city)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.error(err.message);        
+      });
+    }
 
-app.parse(process.argv)
-if (app.args.length === 0) app.help()
+  });
+
+app
+  .command('now')
+  .description('Output of the actual weather of all saved cities')
+  .action(() => {
+    // output
+  });
+
+app.parse(process.argv);
+if (app.args.length === 0) app.help();
