aba61e34b064b84cbadf72132bee4ed62cac23a5
Mirko Schubert <mirkoschubert@gmail.com>
2017-08-26 13:25:32 +0200

    Setting up Ava Test

---
+++ test/config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const test = require('ava');
+const Config = require('../lib/config');
+
+
+test('load default configuration file', t => {
+  const cfg = new Config();
+
+  t.is(typeof (cfg.data), 'object');
+  t.is(typeof (cfg.data.apikey), 'string');
+  t.is(typeof (cfg.data.units), 'string'); 
+  t.is(typeof (cfg.data.temp), 'string');
+  t.is(typeof (cfg.data.cities), 'object');
+
+});
+
+test.todo('try loading non-existant configuration file');
+test.todo('try checking city data');
