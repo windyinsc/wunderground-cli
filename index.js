93f10c9596f75ffeff3e19c82833644cdacf66c0
Mirko Schubert <mirkoschubert@gmail.com>
2017-09-02 23:03:08 +0200

    New UI Part I

--- index.js
+++ index.js
@@ -3,96 +3,42 @@
 
 const Config = require('./lib/config');
 const Wunder = require('./lib/wunderapi');
+const Ui = require('./lib/ui');
 const app = require('commander');
 
 var cfg = new Config();
+var ui = new Ui();
 var wunder = new Wunder(cfg.data.apikey);
 
 app
-  .version('0.0.1')
-  .option('-V, --verbose', 'Switches to Verbose Mode');
+  .version('0.1.1');
 
 app
   .command('init [key]')
   .description('Initializes the Wunderground API')
-  .action(key => {
-
-    if (!key) {
-      console.error(new Error('You should enter a valid API key!'));
-    } else {
-      cfg.saveKey(key)
-      .then(res =>{
-        console.log('Your API key is saved successfully.');
-      })
-      .catch(err => {
-        console.error(err.message);
-      });
-    }
-  });
+  .action(key => ui.init(key));
 
 app
   .command('add [city]')
   .description('Searches for a city and adds it to the favorites')
-  .action((city) => {
-    
-    if (!city) {
-      console.error(new Error('You should enter a City!'));
-    } else {
-      wunder.searchCity(city)
-      .then(res => cfg.saveCity(res))
-      .then(res => {
-        console.log('The city %s is saved!', res.name);
-      })
-      .catch(err => {
-        console.error(err.message);        
-      });
-    }
-
-  });
+  .action(city => ui.add(city));
 
 app
   .command('remove [id]')
   .alias('rm')
   .description('Removes a city from your list')
-  .action(id => {
-
-    if (!id) {
-      console.error(new Error('You should specify the City ID (see: weather list)!'));
-    } else {
-      cfg.deleteCity(id)
-        .then(res => {
-          console.log('\nYour city has been removed.\n');
-        })
-        .catch(err => {
-          console.error(err.message);
-        });
-    }
-  });
+  .action(id => ui.remove(id));
 
 app
   .command('list')
   .description('Lists all cities you have saved')
   .option('-s, --stations', 'Shows the station identifier for each entry')
-  .action((options) => {
-
-    wunder.list(options);
-
-  });
+  .action(options => ui.list(options));
 
 app
   .command('now [id]')
   .description('Output of the actual weather of all saved cities')
-  .option('-i, --imperial', 'Imperial units')
-  .option('-m, --metric', 'Metric Units')
-  .option('-f, --fahrenheit', 'Temperature in °F')
-  .option('-c, --celsius', 'Temperature in °C')
-  .action((id, options) => {
-    if (!id) {
-      wunder.now(cfg.data.cities);
-    } else {
-      wunder.now(cfg.data.cities[id - 1]);
-    }
-  });
+  .action(id => ui.now(id));
 
 app
   .command('alert [id]')
